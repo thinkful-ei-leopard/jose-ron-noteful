0f946f13c5af3e90205feb6d0de78df0c7cff7ff
Jose <jozylopez27@gmail.com>
2020-02-04 15:18:38 -0600

    notes

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
 function Note(props) {
-  // console.log(props.notes)
+  console.log(props.notes)
+const note = props.notes.map(element => 
+<li key={element.id}>{element.name}
+<p>{element.content}</p>
+<p>{element.modified}</p></li>)
   return (
     <div className="noteElement">
-      <li>placeholder Note</li>
+      {note}
     </div>
   )
 }
--- src/components/NoteRoute.js
+++ src/components/NoteRoute.js
@@ -4,15 +4,15 @@ import Note from './Note';
 
 export default function NoteRoute(props) {
     console.log(props.notes)
-    const note = props.notes.map(element => <p>{element.name}</p>)
+    // const note = props.notes.map(element => <p>{element.name}</p>)
     return (
         <>
-        <Note />
-        <p>{note}</p>
+        {/* <p>{note}</p> */}
         <div>
             <button>Go Back</button>
         </div>
         <p>You're in Folder 3</p>
+        <Note notes={props.notes}/>
         </>
     )
 }
