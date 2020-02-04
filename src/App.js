45ba751c72cc2d55f9236bd8d3538b9de9c0fb07
Jose <jozylopez27@gmail.com>
2020-02-04 10:28:08 -0600

    notelist

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import FolderList from './components/FolderList';
+import NoteList from './components/NoteList';
 
 class App extends React.Component {
   state ={
@@ -128,6 +129,7 @@ class App extends React.Component {
     <main className='App'>
       <Header />
       <FolderList />
+      <NoteList />
     </main>
   );
   }
---
+++ src/components/NoteList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default function NoteList() {
+    return (
+        <div>
+            <ul>
+                <li>Hi i am a note!</li>
+            </ul>
+        </div>
+    )
+}
