536099d7fc047f70037da309a90a97f45d1c175d
wavinginspace <paulbaisley@gmail.com>
2020-02-04 11:24:18 -0500

    folderlist component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import FolderList from './components/FolderList';
 
 class App extends React.Component {
   state ={
@@ -126,6 +127,7 @@ class App extends React.Component {
   return (
     <main className='App'>
       <Header />
+      <FolderList />
     </main>
   );
   }
---
+++ src/components/FolderList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function FolderList() {
+  return (
+    <div>
+      <ul>
+        <p>Folders</p>
+      </ul>
+    </div>
+  )
+}
+
+export default FolderList
